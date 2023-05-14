68510ecfb5e79cbfb7bfa9cd7eb7fc369b89cd7a
bogdan <B_Bati@yahoo.com>
2023-05-14 12:07:55 +0300

    By Spring Initializer

---
+++ src/main/java/ro/bb/tranzactii/TranzactiiApplication.java
@@ -0,0 +1,13 @@
+package ro.bb.tranzactii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranzactiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranzactiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/bb/tranzactii/TranzactiiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.bb.tranzactii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranzactiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
